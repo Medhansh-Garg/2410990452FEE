07b66ba28cb58ce6a5a0ec5a279a422fc249a0cb
Medhansh-Garg <medhanshgarg20@gmail.com>
2025-08-25 14:59:56 +0530

    Add basic control flow question

---
+++ jsbasic_controlflow/largest.js
@@ -0,0 +1,13 @@
+let a=19;
+let b=20;
+let c=18;
+if((a>b)&&(a>c)){
+    console.log("largest number:",a);
+}
+else if((b>a)&&(b>c)){
+    console.log("largest number:",b);
+
+}
+else{
+    console.log("largest number",c);
+}
---
+++ jsbasic_controlflow/palindrome.js
@@ -0,0 +1,13 @@
+let b="madam";
+let k="";
+for(i in b){
+    k=b[i]+k;
+
+}
+if(b==k){
+    console.log("palindrome");
+}
+else{
+    console.log("not palindrome");
+
+}
